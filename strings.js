a40eda734ae238fc7428db6152de73734de0b738
Alicia Zaludova <aliciazaludova@gmail.com>
2018-02-26 20:30:56 -0600

    two challenges done

--- strings.js
+++ strings.js
@@ -1,1 +1,10 @@
 console.log("hello world!");
+//challenge 1
+var numOfSandwiches = 4*2;
+console.log("numOfSandwiches", numOfSandwiches);
+// should print 8
+
+//challenge 2
+var name = "Zoe"
+var string = "Hello, " + name + "." + " How are you today?";
+console.log("string:", string);
